52f575fafc8cd944b7a16f0aa7b1589df4a64734
Elvis <s49188705@gmail.com>
2022-10-18 00:23:08 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,50 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet"Naureen"
+greet "Jimmy"
+
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(3, 4)
+
+
+
+def halve (number)
+    if number.class != Integer
+        return nil
+    end
+    number/2
+end
+halve (2)
+halve ("two")
+
+
+
+
